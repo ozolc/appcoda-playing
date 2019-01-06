fc4f6a1c1132096bbfc8a4aa990753b098b5bcca
Maksim Nosov <ozolc@yandex.ru>
2019-01-06 16:11:57 +0300

    Rectangular Shapes

--- PathsNLayers/PathsNLayers/DemoView.swift
+++ PathsNLayers/PathsNLayers/DemoView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DemoView: UIView {
+@IBDesignable class DemoView: UIView {
 
     var path: UIBezierPath!
     
@@ -22,12 +22,36 @@ class DemoView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    func createRectangle() {
+        // Initialize the path.
+        path = UIBezierPath()
+        
+        // Specigy the point that the path should start get drawn.
+        path.move(to: CGPoint(x: 0.0, y: 0.0))
+        
+        // Create a line between the starting point and the bottom-left side of the view.
+        path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
+        
+        // Create the botto, line (bottom-left to bottom-right).
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        
+        // Create the vertical line from the bottom-right to the right side.
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: 0.0))
+        
+        // Close the path. This will create the last line automatically.
+        path.close()
     }
-    */
-
+    
+    override func draw(_ rect: CGRect) {
+        self.createRectangle()
+        
+        // Specify the fill color and apply it to the path.
+        UIColor.orange.setFill()
+        path.fill()
+        
+        path.lineWidth = 5.0
+        // Specify a border (stroke) color.
+        UIColor.purple.setStroke()
+        path.stroke()
+    }
 }

e33cc8a6db1737a5cfa09d1ce02a92afa1bdca7b
Maksim Nosov <ozolc@yandex.ru>
2019-01-06 17:27:23 +0300

    A Simple CAShapeLayer Object

--- PathsNLayers/PathsNLayers/DemoView.swift
+++ PathsNLayers/PathsNLayers/DemoView.swift
@@ -20,56 +20,58 @@ class DemoView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.backgroundColor = UIColor.white
+        self.backgroundColor = UIColor.darkGray
+        
+        simpleShapeLayer()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func draw(_ rect: CGRect) {
-        // self.createRectangle()
-        // self.createTriangle()
-        
-        // Create an oval shape path
-        // self.path = UIBezierPath(ovalIn: self.bounds)
-        
-        // Create an circle shape path
-//        self.path = UIBezierPath(ovalIn: CGRect(x: self.frame.size.width / 2 - self.frame.size.height / 2,
-//                                                y: 0.0,
-//                                                width: self.frame.size.height,
-//                                                height: self.frame.size.height))
-        
-        // Create a rounded rectangle
-        // self.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: 15.0)
-        
-        // Create a rounded rectangle (only top-left and bottom-right corners)
-//        self.path = UIBezierPath(roundedRect: self.bounds,
-//                                 byRoundingCorners: [.topLeft, .bottomRight],
-//                                 cornerRadii: CGSize(width: 15.0, height: 0.0))
-
-        // Create an arc
+//    override func draw(_ rect: CGRect) {
+//        // self.createRectangle()
+//        // self.createTriangle()
+//
+//        // Create an oval shape path
+//        // self.path = UIBezierPath(ovalIn: self.bounds)
+//
+//        // Create an circle shape path
+////        self.path = UIBezierPath(ovalIn: CGRect(x: self.frame.size.width / 2 - self.frame.size.height / 2,
+////                                                y: 0.0,
+////                                                width: self.frame.size.height,
+////                                                height: self.frame.size.height))
+//
+//        // Create a rounded rectangle
+//        // self.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: 15.0)
+//
+//        // Create a rounded rectangle (only top-left and bottom-right corners)
+////        self.path = UIBezierPath(roundedRect: self.bounds,
+////                                 byRoundingCorners: [.topLeft, .bottomRight],
+////                                 cornerRadii: CGSize(width: 15.0, height: 0.0))
+//
+//        // Create an arc
+////        self.path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2),
+////                                 radius: self.frame.size.height / 2,
+////                                 startAngle: CGFloat(deg2rad(180.0)),
+////                                 endAngle: CGFloat(deg2rad(0.0)),
+////                                 clockwise: true)
+//
 //        self.path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2),
 //                                 radius: self.frame.size.height / 2,
-//                                 startAngle: CGFloat(deg2rad(180.0)),
-//                                 endAngle: CGFloat(deg2rad(0.0)),
-//                                 clockwise: true)
-        
-        self.path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2),
-                                 radius: self.frame.size.height / 2,
-                                 startAngle: CGFloat(270.0).toRadians(),
-                                 endAngle: CGFloat(90.0).toRadians(),
-                                 clockwise: false)
-        
-        // Specify the fill color and apply it to the path.
-        UIColor.orange.setFill()
-        path.fill()
-        
-        path.lineWidth = 2.0
-        // Specify a border (stroke) color.
-        UIColor.purple.setStroke()
-        path.stroke()
-    }
+//                                 startAngle: CGFloat(270.0).toRadians(),
+//                                 endAngle: CGFloat(90.0).toRadians(),
+//                                 clockwise: false)
+//
+//        // Specify the fill color and apply it to the path.
+//        UIColor.orange.setFill()
+//        path.fill()
+//
+//        path.lineWidth = 2.0
+//        // Specify a border (stroke) color.
+//        UIColor.purple.setStroke()
+//        path.stroke()
+//    }
     
     func createRectangle() {
         // Initialize the path.
@@ -98,6 +100,20 @@ class DemoView: UIView {
         path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
         path.close()
     }
+    
+    func simpleShapeLayer() {
+        self.createRectangle()
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = self.path.cgPath
+        
+        shapeLayer.fillColor = UIColor.yellow.cgColor
+        shapeLayer.strokeColor = UIColor.brown.cgColor
+        shapeLayer.lineWidth = 3.0
+        
+        self.layer.addSublayer(shapeLayer)
+    }
+    
     
     
 }

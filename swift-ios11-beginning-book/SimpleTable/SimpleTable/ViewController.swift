f79ac578c8ffca8a4bcaf5a0fe5f762a518f5106
Maksim Nosov <ozolc@yandex.ru>
2019-01-23 17:00:48 +0300

    DataSource and Delegate

---
+++ swift-ios11-beginning-book/SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by NosovML on 23/01/2019.
+//  Copyright © 2019 NosovML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+

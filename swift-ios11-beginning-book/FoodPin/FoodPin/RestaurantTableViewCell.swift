6d78a56dea51e4edd281a19c6a220a0903063af4
Maksim Nosov <ozolc@yandex.ru>
2019-01-24 17:59:36 +0300

    Excercise 2

--- swift-ios11-beginning-book/FoodPin/FoodPin/RestaurantTableViewCell.swift
+++ swift-ios11-beginning-book/FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -13,12 +13,13 @@ class RestaurantTableViewCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var locationLabel: UILabel!
     @IBOutlet var typeLabel: UILabel!
-    @IBOutlet var thumbnailImageView: UIImageView! {
-        didSet {
-            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
-            thumbnailImageView.clipsToBounds = true
-        }
-    }
+    @IBOutlet var thumbnailImageView: UIImageView!
+//        {
+//        didSet {
+//            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+//            thumbnailImageView.clipsToBounds = true
+//        }
+//    }
 
     override func awakeFromNib() {
         super.awakeFromNib()

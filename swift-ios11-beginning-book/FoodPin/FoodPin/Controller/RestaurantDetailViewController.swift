ddc3e7af2c1990fb1be9e53564730eaecb9f3324
Maksim Nosov <ozolc@yandex.ru>
2019-01-28 18:09:12 +0300

    Designing the Table View Header (fix errors later)

--- swift-ios11-beginning-book/FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
+++ swift-ios11-beginning-book/FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -10,33 +10,26 @@ import UIKit
 
 class RestaurantDetailViewController: UIViewController {
     
-    @IBOutlet var restaurantImageView: UIImageView!
-    @IBOutlet var restaurantNameLabel: UILabel!
-    @IBOutlet var restaurantTypeLabel: UILabel!
-    @IBOutlet var restaurantLocationLabel: UILabel!
+//    @IBOutlet var restaurantImageView: UIImageView!
+//    @IBOutlet var restaurantNameLabel: UILabel!
+//    @IBOutlet var restaurantTypeLabel: UILabel!
+//    @IBOutlet var restaurantLocationLabel: UILabel!
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
     
     var restaurant: Restaurant = Restaurant()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        restaurantImageView.image = UIImage(named: restaurant.image)
-        restaurantNameLabel.text = restaurant.name
-        restaurantTypeLabel.text = restaurant.type
-        restaurantLocationLabel.text = restaurant.location
+        headerView.heartImageView.image = UIImage(named: restaurant.image)
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+//        headerView.restaurantLocationLabel.text = restaurant.location
+        headerView.heartImageView.isHidden = (restaurant.isVisited) ? false : true
         
         navigationItem.largeTitleDisplayMode = .never
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ swift-ios11-beginning-book/FoodPin/FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by NosovML on 28/01/2019.
+//  Copyright © 2019 NosovML. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    @IBOutlet var headerImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var heartImageView: UIImageView!
+
+}

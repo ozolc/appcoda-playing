8d543217652e9309c3b09df607f3cfda1cec3eb5
Maksim Nosov <ozolc@yandex.ru>
2019-02-15 18:30:07 +0300

    Create a Segue for the Modal View

--- swift-ios11-beginning-book/FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
+++ swift-ios11-beginning-book/FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -104,6 +104,11 @@ extension RestaurantDetailViewController: UITableViewDataSource {
         if segue.identifier == "showMap" {
             let destinationController = segue.destination as! MapViewController
             destinationController.restaurant = restaurant
+        } else {
+            if segue.identifier == "showReview" {
+                let destinationController = segue.destination as! ReviewViewController
+                destinationController.restaurant = restaurant
+            }
         }
     }
     

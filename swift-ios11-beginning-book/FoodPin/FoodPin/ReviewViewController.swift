3dbde64ed143c1b0c3001c65d068f616e1526978
Maksim Nosov <ozolc@yandex.ru>
2019-02-16 16:06:34 +0300

    Exercise #2

--- swift-ios11-beginning-book/FoodPin/FoodPin/ReviewViewController.swift
+++ swift-ios11-beginning-book/FoodPin/FoodPin/ReviewViewController.swift
@@ -15,7 +15,7 @@ class ReviewViewController: UIViewController {
     @IBOutlet var closeButton: UIButton!
     
     var restaurant = Restaurant()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -44,42 +44,36 @@ class ReviewViewController: UIViewController {
         // Close button animatiom
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        UIView.animate(withDuration: 2.0) {
-//            self.rateButtons[0].alpha = 1.0
-//            self.rateButtons[1].alpha = 1.0
-//            self.rateButtons[2].alpha = 1.0
-//            self.rateButtons[3].alpha = 1.0
-//            self.rateButtons[4].alpha = 1.0
-//        }
-//    }
-    
     override func viewWillAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 0.4, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
-            self.rateButtons[0].alpha = 1.0
-            self.rateButtons[0].transform = .identity
-        }, completion: nil)
-        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
-            self.rateButtons[1].alpha = 1.0
-            self.rateButtons[1].transform = .identity
-        }, completion: nil)
-        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
-            self.rateButtons[2].alpha = 1.0
-            self.rateButtons[2].transform = .identity
-        }, completion: nil)
-        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
-            self.rateButtons[3].alpha = 1.0
-            self.rateButtons[3].transform = .identity
-        }, completion: nil)
-        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
-            self.rateButtons[4].alpha = 1.0
-            self.rateButtons[4].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
-            self.closeButton.alpha = 1.0
-            self.closeButton.transform = .identity
-        }, completion: nil)
+        var step = 0.1
+        for button in self.rateButtons {
+            UIView.animate(withDuration: 0.4, delay: step , usingSpringWithDamping: 0.3, initialSpringVelocity: 0.1, options: [], animations: {
+                button.alpha = 1.0
+                button.transform = .identity
+            }, completion: nil)
+            step += 0.05
+            //        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+            //            self.rateButtons[1].alpha = 1.0
+            //            self.rateButtons[1].transform = .identity
+            //        }, completion: nil)
+            //        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
+            //            self.rateButtons[2].alpha = 1.0
+            //            self.rateButtons[2].transform = .identity
+            //        }, completion: nil)
+            //        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
+            //            self.rateButtons[3].alpha = 1.0
+            //            self.rateButtons[3].transform = .identity
+            //        }, completion: nil)
+            //        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
+            //            self.rateButtons[4].alpha = 1.0
+            //            self.rateButtons[4].transform = .identity
+            //        }, completion: nil)
+            
+            UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
+                self.closeButton.alpha = 1.0
+                self.closeButton.transform = .identity
+            }, completion: nil)
+        }
     }
-
+    
 }

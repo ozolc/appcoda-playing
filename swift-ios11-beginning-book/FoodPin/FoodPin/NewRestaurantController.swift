7c6078b99a26f8b1cd620938ed4b544f3e75f032
Maksim Nosov <ozolc@yandex.ru>
2019-02-16 17:54:22 +0300

    Customizing the Navigation Bar

--- swift-ios11-beginning-book/FoodPin/FoodPin/NewRestaurantController.swift
+++ swift-ios11-beginning-book/FoodPin/FoodPin/NewRestaurantController.swift
@@ -51,6 +51,15 @@ class NewRestaurantController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 35.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60),
+                NSAttributedString.Key.font: customFont
+            ]
+        }
         
     }
 

05ac23f918f7121aede07deceb2568c0c026734f
Maksim Nosov <ozolc@yandex.ru>
2019-02-16 15:09:08 +0300

    Animating Dialog View Using UIView Animation

--- swift-ios11-beginning-book/FoodPin/FoodPin/ReviewViewController.swift
+++ swift-ios11-beginning-book/FoodPin/FoodPin/ReviewViewController.swift
@@ -25,6 +25,39 @@ class ReviewViewController: UIViewController {
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
+        
+        // Make the button invisible
+        for rateButton in rateButtons {
+            rateButton.alpha = 0
+        }
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        UIView.animate(withDuration: 2.0) {
+//            self.rateButtons[0].alpha = 1.0
+//            self.rateButtons[1].alpha = 1.0
+//            self.rateButtons[2].alpha = 1.0
+//            self.rateButtons[3].alpha = 1.0
+//            self.rateButtons[4].alpha = 1.0
+//        }
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
+            self.rateButtons[0].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+            self.rateButtons[1].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
+            self.rateButtons[2].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
+            self.rateButtons[3].alpha = 1.0
+        }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
+            self.rateButtons[4].alpha = 1.0
+        }, completion: nil)
     }
 
 }

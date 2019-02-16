464f5965eb0f9735836026d61d8812486307e184
Maksim Nosov <ozolc@yandex.ru>
2019-02-16 17:09:33 +0300

    Linking Up the New Restaurant Controller

--- swift-ios11-beginning-book/FoodPin/FoodPin/Controller/RestaurantTableViewController.swift
+++ swift-ios11-beginning-book/FoodPin/FoodPin/Controller/RestaurantTableViewController.swift
@@ -182,4 +182,8 @@ class RestaurantTableViewController: UITableViewController {
         }
     }
     
+    @IBAction func unwindToHome(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
